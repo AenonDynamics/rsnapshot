903c16f9744e5198526dea77a909e6acd9cce4d7
Andi Dittrich <andi@andidittrich.de>
2017-10-28 15:46:23 +0000

    show npm/node version on ci install

--- test/install-nodejs.sh
+++ test/install-nodejs.sh
@@ -7,4 +7,14 @@ wget https://nodejs.org/dist/v8.8.1/node-v8.8.1-linux-x64.tar.gz -O /tmp/nodejs.
 tar -xzf /tmp/nodejs.tgz
 
 # add nodejs binaries to path
-export PATH=$PATH:$TRAVIS_BUILD_DIR/node-v8.8.1-linux-x64/bin
+export PATH=$TRAVIS_BUILD_DIR/node-v8.8.1-linux-x64/bin:$PATH
+
+# show path
+echo "Current Path: $PATH"
+
+# show nodejs version
+echo "Node.js Version:"
+node -v
+
+echo "NPM Version:"
+npm -v
