3de142b5887f454758607e5ebbb53cc86e6edd81
Andi Dittrich <andi@andidittrich.de>
2017-10-29 18:39:24 +0000

    added rsync test; added cp test; added diff based compareDirectory test helper

--- test/21_cp.test.js
+++ test/21_cp.test.js
@@ -2,7 +2,7 @@ const _lib = require('./lib');
 const _path = require('path');
 const _assert = require('assert');
 
-describe('rsync', function(){
+describe('copy:gnu_cp/buildin', function(){
 
     it('should pass - 1st snapshot using GNU cp', async function(){
         await _lib.rsnapshotDynamicConfig([
@@ -23,6 +23,34 @@ describe('rsync', function(){
         await _lib.rsnapshotDynamicConfig([
             'config_version 1.3',
             `cmd_cp ${_lib.bin.cp}`,
+            `cmd_rsync ${_lib.bin.rsync}`,
+            `snapshot_root ${_lib.path.snapshotRoot}`,
+            'retain alpha 6',
+            `backup ${_lib.path.data} local`
+        ], 'alpha');
+
+        // validate integrity
+        const equals = await _lib.compareDirectories(_lib.path.data, _path.join(_lib.path.snapshotRoot, 'alpha.0/local', _lib.path.data));
+        _assert.equal(equals, true);
+    });
+
+    it('should pass - 3rd snapshot using internal cp', async function(){
+        await _lib.rsnapshotDynamicConfig([
+            'config_version 1.3',
+            `cmd_rsync ${_lib.bin.rsync}`,
+            `snapshot_root ${_lib.path.snapshotRoot}`,
+            'retain alpha 6',
+            `backup ${_lib.path.data} local`
+        ], 'alpha');
+
+        // validate integrity
+        const equals = await _lib.compareDirectories(_lib.path.data, _path.join(_lib.path.snapshotRoot, 'alpha.0/local', _lib.path.data));
+        _assert.equal(equals, true);
+    });
+
+    it('should pass - 4th snapshot using internal cp', async function(){
+        await _lib.rsnapshotDynamicConfig([
+            'config_version 1.3',
             `cmd_rsync ${_lib.bin.rsync}`,
             `snapshot_root ${_lib.path.snapshotRoot}`,
             'retain alpha 6',
